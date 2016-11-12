8fbced6b23ea08188e5295330405108f565b0a17
Akshay Thapa <akshay.thapa23@gmail.com>
2016-11-12 19:00:20 +0530

    ProductsActivity setup

--- TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/presentation/view/activity/ProductsActivity.java
+++ TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/presentation/view/activity/ProductsActivity.java
@@ -1,15 +1,89 @@
 package com.rubberduck.transactionsviewer.presentation.view.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.rubberduck.transactionsviewer.R;
+import com.rubberduck.transactionsviewer.presentation.internal.di.component.DaggerProductComponent;
+import com.rubberduck.transactionsviewer.presentation.internal.di.component.ProductComponent;
+import com.rubberduck.transactionsviewer.presentation.model.ProductViewModel;
+import com.rubberduck.transactionsviewer.presentation.presenter.ProductsPresenter;
 
-public class ProductsActivity extends AppCompatActivity {
+import java.util.List;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+
+public class ProductsActivity extends MvpActivity<ProductsPresenter> implements ProductsPresenter.View {
+
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
+
+    @BindView(R.id.products_recycler)
+    RecyclerView productsRecycler;
+
+    @BindView(R.id.error_text_view)
+    TextView errorTextView;
+
+    @Inject
+    ProductsPresenter productsPresenter;
+
+    private ProductComponent productComponent;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_products);
+        initProductsRecycler();
+    }
+
+    private void initProductsRecycler() {
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_products;
+    }
+
+    @Override
+    protected void initActivity(Bundle bundle) {
+    }
+
+    @Override
+    protected void injectPresenter() {
+        productComponent = DaggerProductComponent.builder()
+                                                 .applicationComponent(getApplicationComponent())
+                                                 .build();
+        productComponent.inject(this);
+    }
+
+    @Override
+    protected ProductsPresenter getPresenter() {
+        return productsPresenter;
+    }
+
+    @Override
+    public void showProgressBar() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgressBar() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showProducts(List<ProductViewModel> products) {
+
+    }
+
+    @Override
+    public void showError(String errorMessage) {
+        errorTextView.setText(errorMessage);
+        errorTextView.setVisibility(View.VISIBLE);
     }
 }
