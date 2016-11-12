f89ca8f615d5ee30ca5b52bbe2a597d8cbf641ae
Akshay Thapa <akshay.thapa23@gmail.com>
2016-11-12 17:14:49 +0530

    Added NoProductAvailableException

---
+++ TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/domain/exception/NoProductAvailableException.java
@@ -0,0 +1,8 @@
+package com.rubberduck.transactionsviewer.domain.exception;
+
+public class NoProductAvailableException extends Exception {
+
+    public NoProductAvailableException(String message) {
+        super(message);
+    }
+}
