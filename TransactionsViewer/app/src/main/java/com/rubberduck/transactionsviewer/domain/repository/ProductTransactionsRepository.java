c1efcf18a953e0ad0c7fe4c9f593a7501776bcfd
Akshay Thapa <akshay.thapa23@gmail.com>
2016-11-12 19:30:30 +0530

    Added getAllTransactions method in ProductTransactionsRepository

--- TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/domain/repository/ProductTransactionsRepository.java
+++ TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/domain/repository/ProductTransactionsRepository.java
@@ -3,6 +3,7 @@ package com.rubberduck.transactionsviewer.domain.repository;
 import com.rubberduck.transactionsviewer.domain.exception.NoProductsAvailableException;
 import com.rubberduck.transactionsviewer.domain.exception.ProductNotFoundException;
 import com.rubberduck.transactionsviewer.domain.model.Product;
+import com.rubberduck.transactionsviewer.domain.model.Transaction;
 
 import java.util.List;
 
@@ -27,5 +28,14 @@ public interface ProductTransactionsRepository {
      * @throws ProductNotFoundException If a product with the given sku is not found
      */
     Integer getTransactionsCount(String productSku) throws ProductNotFoundException;
+
+    /**
+     * Returns a list of all the transactions for a particular {@link Product}.
+     *
+     * @param productSku The sku of the product whose transactions are required
+     * @return A list of {@link Transaction} for the given product, or an empty list if no transactions are present
+     * @throws ProductNotFoundException If a product with the given sku is not found
+     */
+    List<Transaction> getAllTransactions(String productSku) throws ProductNotFoundException;
 
 }
