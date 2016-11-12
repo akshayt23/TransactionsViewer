de046c220a94a46ccd854e4b5bcd417168eda019
Akshay Thapa <akshay.thapa23@gmail.com>
2016-11-12 18:30:44 +0530

    Renamed MainActivity to ProductsActivity

---
+++ TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/presentation/view/activity/ProductsActivity.java
@@ -0,0 +1,15 @@
+package com.rubberduck.transactionsviewer.presentation.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rubberduck.transactionsviewer.R;
+
+public class ProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+    }
+}
