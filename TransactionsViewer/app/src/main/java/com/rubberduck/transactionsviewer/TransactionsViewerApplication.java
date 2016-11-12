a50c8712e25e5791eeedc7d89ed3155aacd1c2c8
Akshay Thapa <akshay.thapa23@gmail.com>
2016-11-12 18:49:13 +0530

    Added ApplicationComponent to Application class

--- TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/TransactionsViewerApplication.java
+++ TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/TransactionsViewerApplication.java
@@ -2,10 +2,28 @@ package com.rubberduck.transactionsviewer;
 
 import android.app.Application;
 
+import com.rubberduck.transactionsviewer.presentation.internal.di.component.ApplicationComponent;
+import com.rubberduck.transactionsviewer.presentation.internal.di.component.DaggerApplicationComponent;
+import com.rubberduck.transactionsviewer.presentation.internal.di.module.ApplicationModule;
+
 public class TransactionsViewerApplication extends Application {
+
+    private ApplicationComponent applicationComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        initInjector();
+    }
+
+    private void initInjector() {
+        applicationComponent = DaggerApplicationComponent
+                .builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+    }
+
+    public ApplicationComponent getApplicationComponent() {
+        return applicationComponent;
     }
 }
