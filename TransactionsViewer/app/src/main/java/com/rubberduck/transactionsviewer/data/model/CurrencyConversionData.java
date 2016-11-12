f580bdcdc90475e50e62278bcd4c174f61ea2658
Akshay Thapa <akshay.thapa23@gmail.com>
2016-11-12 20:05:03 +0530

    Added CurrencyConversionData model

---
+++ TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/data/model/CurrencyConversionData.java
@@ -0,0 +1,20 @@
+package com.rubberduck.transactionsviewer.data.model;
+
+public class CurrencyConversionData {
+
+    private String from;
+    private double rate;
+    private String to;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public String getTo() {
+        return to;
+    }
+}
