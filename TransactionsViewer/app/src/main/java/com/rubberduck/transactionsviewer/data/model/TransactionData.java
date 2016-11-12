467e30cda098732e30d7199f9f01c613980f7f78
Akshay Thapa <akshay.thapa23@gmail.com>
2016-11-12 17:53:10 +0530

    Added TransactionData model

---
+++ TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/data/model/TransactionData.java
@@ -0,0 +1,21 @@
+package com.rubberduck.transactionsviewer.data.model;
+
+public class TransactionData {
+
+    private String sku;
+    private String currency;
+    private double amount;
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+}
