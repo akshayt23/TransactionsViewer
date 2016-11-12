fd0a165477643c5780c1b0ae84d0b9bdc320dafe
Akshay Thapa <akshay.thapa23@gmail.com>
2016-11-12 18:07:54 +0530

    Added application class

---
+++ TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/TransactionsViewerApplication.java
@@ -0,0 +1,11 @@
+package com.rubberduck.transactionsviewer;
+
+import android.app.Application;
+
+public class TransactionsViewerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
