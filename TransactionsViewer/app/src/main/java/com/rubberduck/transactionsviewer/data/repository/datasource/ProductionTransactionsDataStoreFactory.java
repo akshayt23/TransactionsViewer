a103145b1174c016c24c843899845f3c3a7fa49f
Akshay Thapa <akshay.thapa23@gmail.com>
2016-11-12 17:55:12 +0530

    Added data store factory

---
+++ TransactionsViewer/app/src/main/java/com/rubberduck/transactionsviewer/data/repository/datasource/ProductionTransactionsDataStoreFactory.java
@@ -0,0 +1,30 @@
+package com.rubberduck.transactionsviewer.data.repository.datasource;
+
+import android.content.Context;
+
+import com.rubberduck.transactionsviewer.data.serializer.JsonSerializer;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class ProductionTransactionsDataStoreFactory {
+
+    private final Context context;
+    private final JsonSerializer jsonSerializer;
+
+    @Singleton
+    public ProductionTransactionsDataStoreFactory(Context context, JsonSerializer jsonSerializer) {
+        this.context = context;
+        this.jsonSerializer = jsonSerializer;
+    }
+
+    /**
+     * Returns a local/remote data store depending on where the required data can be found.
+     * For now, we'll just return a local implementation of {@link ProductTransactionsDataStore}.
+     *
+     * @return A {@link ProductTransactionsDataStore}
+     */
+    public ProductTransactionsDataStore create() {
+        return new FileProductTransactionsDataStore(context, jsonSerializer);
+    }
+}
